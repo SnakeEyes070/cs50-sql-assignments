9a1aabd6d88a25cea73409fe92a0a928ad64b63b
SnakeEyes070 <sankalpp608@gmail.com>
2026-02-27 18:00:00 +0530

    Completed players queries

---
+++ SnakeEyes070-cs50-problems-2024-sql-players/10.sql
@@ -0,0 +1,3 @@
+SELECT first_name, last_name, height AS "Height in Inches" FROM players
+WHERE birth_country = 'USA'
+ORDER BY height DESC;
---
+++ SnakeEyes070-cs50-problems-2024-sql-players/6.sql
@@ -0,0 +1,3 @@
+SELECT debut, first_name, last_name FROM players
+WHERE birth_city = "Pittsburgh" AND birth_state = "PA"
+ORDER BY debut DESC, first_name ASC, last_name ASC ;

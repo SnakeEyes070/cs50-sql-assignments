cf50a81279c9ea43b6a8f6559e35d6e6456e7686
SnakeEyes070 <sankalpp608@gmail.com>
2026-03-01 09:20:00 +0530

    Solved packages problem

---
+++ SnakeEyes070-cs50-problems-2024-sql-packages/log.sql
@@ -0,0 +1,32 @@
+
+-- *** The Lost Letter ***
+SELECT * FROM addresses WHERE address = '900 Somerville Avenue';
+
+SELECT * FROM addresses WHERE address LIKE '%2 finn%';
+
+SELECT * FROM scans WHERE package_id = ( SELECT id FROM packages
+WHERE from_address_id = ( SELECT id FROM addresses WHERE address = '900 Somerville Avenue')
+AND to_address_id = ( SELECT id FROM addresses WHERE address LIKE '%2 finn%')
+);
+
+
+-- *** The Devious Delivery ***
+
+SELECT contents FROM packages WHERE from_address_id IS NULL;
+
+SELECT type FROM addresses
+WHERE id = ( SELECT address_id FROM scans WHERE package_id = ( SELECT id FROM packages
+WHERE from_address_id IS NULL)
+    ORDER BY timestamp DESC
+    LIMIT 1
+);
+
+-- *** The Forgotten Gift ***
+
+SELECT contents FROM packages WHERE from_address_id = ( SELECT id FROM addresses WHERE address = '109 Tileston Street')
+AND to_address_id = ( SELECT id FROM addresses WHERE address = '728 Maple Place');
+
+
+SELECT name FROM drivers WHERE id = (SELECT driver_id FROM scans WHERE package_id = (
+SELECT id FROM packages WHERE from_address_id = ( SELECT id FROM addresses WHERE address = '109 Tileston Street')
+AND to_address_id = ( SELECT id FROM addresses WHERE address = '728 Maple Place') )ORDER BY "timestamp" DESC) ;
